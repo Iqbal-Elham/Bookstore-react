aa3f16e672af0a1164cf5d5daa7651ea5d576286
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-02 22:52:41 +0430

    Add Categories component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './styles/Categories.module.css'
+const Categories = () => {
+    return (
+        <div className={styles.category}>
+            <button>Check Status</button>
+        </div>
+    );
+}
+
+export default Categories;

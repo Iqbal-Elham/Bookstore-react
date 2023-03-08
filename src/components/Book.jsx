100b6637cfd8fd3ee331a36b235f2cf810462dd9
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-08 20:27:13 +0430

    Display books from slice

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import styles from './styles/Book.module.css';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = ({ title = '', author = '' }) => (
-  <div className={styles.container}>
-    <h1>{title}</h1>
-    <p>{author}</p>
-    <div className={styles.buttons}>
-      <button type="button">Comments</button>
-      <button type="button">Edit</button>
-      <button type="button">Remove</button>
+const Book = ({
+  id, title, author, category,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <div className={styles.container}>
+      <h1>{ title }</h1>
+      <p>{ author }</p>
+      <p>{ category }</p>
+      <div className={styles.buttons}>
+        <button type="button">Comments</button>
+        <button type="button">Edit</button>
+        <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Book.defaultProps = {
+  id: '',
+};
 
 Book.propTypes = {
+  id: PropTypes.string,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;

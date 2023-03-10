9a88363718a54cd9bfa05380b5a8cd6d2aa7bf26
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-10 23:24:59 +0430

    Add remove from api button in book comp

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import styles from './styles/Book.module.css';
-import { removeBook } from '../redux/books/booksSlice';
+import { removeBook, deleteBook } from '../redux/books/booksSlice';
 
-const Book = ({
-  id, title, author,
-}) => {
+const Book = ({ id, title, author }) => {
   const dispatch = useDispatch();
   return (
     <div className={styles.container}>
-      <h1>{ title }</h1>
-      <p>{ author }</p>
+      <h1>{title}</h1>
+      <p>{author}</p>
       <div className={styles.buttons}>
         <button type="button">Comments</button>
         <button type="button">Edit</button>
-        <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(removeBook(id));
+            dispatch(deleteBook(id));
+          }}
+        >
+          Remove
+        </button>
       </div>
     </div>
   );
